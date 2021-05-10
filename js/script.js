7604bc72c7fe1217b889ac43639911f629888f79
Devon Webb <devonwebb90@gmail.com>
2021-05-10 08:47:08 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", function(){
+  /*PAGELOADER*/
+  document.querySelector(".page-loader").classList.add("fade-out")
+  setTimeout(function(){
+    document.querySelector(".page-loader").style.display ="none"
+  }, 600);
+  /*SCROLL ANIMATION*/
+    AOS.init();
+});
+
+/* toggle navbar */
+const navToggler = document.querySelector(".nav-toggle");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+  navToggler.classList.toggle("active");
+  document.querySelector(".nav").classList.toggle("open");
+}
+
+/*Close navBar*/
+document.addEventListener("click", function(e){
+  if(e.target.closest(".nav-item")){
+    toggleNav();
+  }
+} );
+
+/*Stick the header*/
+window.addEventListener("scroll", function(){
+  if(this.pageYOffset > 50) {
+    document.querySelector(".header").classList.add("sticky");
+  } else {
+    document.querySelector(".header").classList.remove("sticky");
+  }
+});
+
+/* menu tabs */
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+  if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+    const target = e.target.getAttribute("data-target");
+    menuTabs.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    const menuSection = document.querySelector(".menu-section");
+    menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+    menuSection.querySelector(target).classList.add("active");
+    /*SCROLL ANIMATION*/
+    AOS.init();
+  }
+});
